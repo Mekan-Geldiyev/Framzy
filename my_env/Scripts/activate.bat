7cd7b467ddf7e5073dc96888c14c7025b7946bef
shadowmaster2900 <97012609+shadowmaster2900@users.noreply.github.com>
2024-10-20 16:37:33 -0700

    removed old virtualenv
    removed old virtualenv  and made new one and updated the requirements.txt

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Program Files\Git\instagram\my_env
+set VIRTUAL_ENV=C:\Users\mekan\OneDrive\Documents\GitHub\Framzy\my_env
 
 if not defined PROMPT set PROMPT=$P$G
 
